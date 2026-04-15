5d6eea71ef37f92ff87c6f80c5e17b59bd639673
ArgyTataMaulana <halmaheru37@gmail.com>
2026-04-16 00:47:00 +0700

    menambahkan class Koneksi.cs fungsinya untuk menghubungkan ke database MySql

--- MadingDigital/Form1.cs
+++ MadingDigital/Form1.cs
@@ -7,22 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient; 
 
 
 namespace MadingDigital
 {
-    class Koneksi
-    {
-        public MySqlConnection GetConn()
-        {
-            // menghubungkan ke database MySql
-            string connString = "server=localhost;database=madingDigital_DB;uid=root;pwd=Satoru12345;";
-            MySqlConnection conn = new MySqlConnection(connString);
-            return conn;
-        }
-    }
-
     public partial class Form1 : Form
     {
         public Form1()
---
+++ MadingDigital/Koneksi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace MadingDigital
+{
+    class Koneksi
+    {
+        public MySqlConnection GetConn()
+        {
+            // untuk menghubungkan ke database
+            string connString = "server=localhost;database=madingDigital;uid=root;pwd=Satoru12345;";
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
+    }
+}
