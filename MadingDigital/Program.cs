9f6aa8c16bd82555a535e600e2417c5a8a890fcf
ArgyTataMaulana <halmaheru37@gmail.com>
2026-04-16 07:18:45 +0700

    supaya yang muncul pertama kali adalah koneksi.cs

--- MadingDigital/Program.cs
+++ MadingDigital/Program.cs
@@ -16,7 +16,7 @@ namespace MadingDigital
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormKoneksi());
         }
     }
 }
